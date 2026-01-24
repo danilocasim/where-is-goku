3dacb9884bc59d43c58082030958c3e913a2b867
Danilo Casim Jr. <danilocasim174@gmail.com>
2026-01-24 19:30:19 +0800

    feat: check coordinates for the selected characters

--- client/src/components/Dropdown/Dropdown.jsx
+++ client/src/components/Dropdown/Dropdown.jsx
@@ -1,19 +1,36 @@
 import style from './Dropdown.module.css';
 
-function Dropdown({ posX, posY }) {
+function Dropdown({ posX, posY, xStyle, yStyle, setPos }) {
+  const API_URL = 'http://localhost:8000/api/v1';
+
+  function checkCoordinates(name) {
+    fetch(`${API_URL}/checkCoordinates`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors',
+      body: JSON.stringify({
+        name: name,
+        coordinateX: posX,
+        coordinateY: posY,
+      }),
+      method: 'POST',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPos(null);
+      });
+  }
+
   return (
     <nav
       className={style.dropdown}
-      style={{ top: posY + 'px', left: posX + 'px' }}
+      style={{ top: yStyle + 'px', left: xStyle + 'px' }}
     >
-      <div>
-        <div>O</div>
-        <div>Goku</div>
-      </div>
-      <div>
-        <div>O</div>
-        <div>Mojo Jojo</div>
-      </div>{' '}
+      <button onClick={() => checkCoordinates('Goku')}>Goku</button>
+      <button onClick={() => checkCoordinates('Mojo Jojo')}>Mojo Jojo</button>
+      <button onClick={() => checkCoordinates('Leonardo')}>Leonardo</button>
     </nav>
   );
 }
