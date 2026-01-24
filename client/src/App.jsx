540ed66ca560326a94d7e40e68b0dfbd5eb5a998
Danilo Casim Jr. <danilocasim174@gmail.com>
2026-01-24 19:29:41 +0800

    feat: fetch the session for getting the user session

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
 import { Outlet } from 'react-router';
-
 import Navigation from './components/Navigation/Navigation';
 import style from './App.module.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  console.log(characters);
+
+  const API_URL = 'http://localhost:8000/api/v1';
+
+  useEffect(() => {
+    fetch(`${API_URL}/sessions`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data, '2');
+        setCharacters(data.message.user.chars);
+      });
+  }, []);
+
   return (
     <div className={style.container}>
       <Navigation></Navigation>
