99abba01b592ba053653bb4046ff9e45316763f3
Danilo Casim Jr. <danilocasim174@gmail.com>
2026-01-27 16:38:30 +0800

    fix: cors issue for sending the credentials of the user to server

--- api/app.js
+++ api/app.js
@@ -6,16 +6,32 @@ import session from 'express-session';
 import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 import { PrismaClient } from './generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { randomUUID } from 'crypto';
 const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
 const app = express();
 
-app.use(cors());
+const whitelist = ['http://localhost:5173' /** other domains if any */];
+const corsOptions = {
+  credentials: true,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(
   session({
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, // Cookie expiration: 1 day
+    },
     secret: 'a santa at nasa',
     resave: false,
     saveUninitialized: false,
