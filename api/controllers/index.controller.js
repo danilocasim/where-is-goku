822de3d8a03dd825f6f84b80986f259b37aeb6f2
Danilo Casim Jr. <danilocasim174@gmail.com>
2026-01-27 16:38:49 +0800

    feat: add a logic for removing the chars if correct

--- api/controllers/index.controller.js
+++ api/controllers/index.controller.js
@@ -3,8 +3,11 @@ import db from '../queries/index.queries.js';
 class Index {
   async checkCoordinates(req, res) {
     const { name, coordinateX, coordinateY } = req.body;
-    console.log(req.session);
     if (await db.checkCoordinates(name, coordinateX, coordinateY)) {
+      if (req.session.user.chars.includes(name)) {
+        const index = req.session.user.chars.indexOf(name);
+        req.session.user.chars.splice(index, 1);
+      }
       res.json({ message: 'Correct', session: req.session });
     } else {
       res.json({ message: 'Wrong', session: req.session });
