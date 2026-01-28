290d5e027598ca896a3e7b83b481cde634f9c687
Danilo Casim Jr. <danilocasim174@gmail.com>
2026-01-28 17:21:13 +0800

    feat: add another props in CharacterContext

--- client/src/contexts/CharacterContext.jsx
+++ client/src/contexts/CharacterContext.jsx
@@ -3,4 +3,9 @@ import { createContext } from 'react';
 export const CharacterContext = createContext({
   characters: [],
   setCharacters: () => {},
+  setStopInterval: () => {},
+  setSession: () => {},
+  setSecondsPassed: () => {},
+  showAddPlayer: false,
+  setShowAddPlayer: () => {},
 });
