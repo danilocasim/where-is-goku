0c81af6e1d3e6781add395f194f860aede205791
Danilo Casim Jr. <danilocasim174@gmail.com>
2026-01-28 17:19:07 +0800

    feat: add leaderboard pages for getting all of the players

--- client/src/pages/LeaderboardPage/LeaderboardPage.jsx
+++ client/src/pages/LeaderboardPage/LeaderboardPage.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react';
+import style from './LeaderboardPage.module.css';
+import { Link } from 'react-router';
+
 function LeaderboardPage() {
-  return <h1>Leaderboard</h1>;
+  const [leaderboard, setLeaderboard] = useState([]);
+  const API_URL = 'http://localhost:8000/api/v1';
+
+  useEffect(() => {
+    fetch(`${API_URL}/leaderboard`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors',
+    })
+      .then((response) => response.json())
+      .then((data) => setLeaderboard(data));
+  }, []);
+
+  return (
+    <div className={style.leaderboards}>
+      <Link to={'/'}>Go back to home</Link>
+      <br />
+      Top Players:
+      {leaderboard.leaderboard &&
+        leaderboard.leaderboard.map((player, index) => {
+          return (
+            <p key={index}>
+              {index + 1}: {player.name} --- {player.timeDisplay}
+            </p>
+          );
+        })}
+    </div>
+  );
 }
 
 export default LeaderboardPage;
